47f4981b4cde908ec0676a1da5bc95ef3d2bcd13
Culturology's Undead <a.pawluczenko@gmail.com>
2025-12-04 18:22:32 +0300

    try new providers

--- dev/draft.R
+++ dev/draft.R
@@ -62,3 +62,42 @@ res <- llm_stance(
     domain_role = 'политический обозреватель',
     verbose = TRUE
 )
+
+### MWS ----
+oc_credentials <- function() {
+    list(Authorization = paste(
+        'Bearer', Sys.getenv('HUGGINGFACE_API_KEY')
+    ))
+}
+
+chat_base <- chat_openai_compatible(
+    base_url = 'https://router.huggingface.co/v1',
+    # name = 'Featherless AI',
+    name = 'novita',
+    credentials = oc_credentials,
+    # model = 'MTSAIR/Cotype-Nano:featherless-ai',
+    model = 'deepseek-ai/DeepSeek-V3.2:novita',
+    api_args = list(temperature = 0)
+)
+
+chat_base$chat('Здравствуйте', echo = 'none')
+
+# Yandex ----
+YANDEX_CLOUD_FOLDER = "b1gsm63ta97sfo8jpbgu"
+# YANDEX_CLOUD_API_KEY = "<API_key_value>"
+YANDEX_CLOUD_MODEL = "aliceai-llm/latest"
+
+ya_credentials <- function() {
+    list(Authorization = paste(
+        'Api-Key', Sys.getenv('YANDEX_API_KEY')
+    ))
+}
+
+chat_base <- chat_openai_compatible(
+    base_url="https://rest-assistant.api.cloud.yandex.net/v1",
+    name = '',
+    credentials = ya_credentials,
+    # model = glue::glue('gpt://{YANDEX_CLOUD_FOLDER}/{YANDEX_CLOUD_MODEL}'),
+    model = 'gpt://b1gsm63ta97sfo8jpbgu/aliceai-llm/latest',
+    api_headers = c('OpenAI-Project' = YANDEX_CLOUD_FOLDER)
+)
